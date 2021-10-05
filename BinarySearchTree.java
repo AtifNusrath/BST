167065555e47889bc4a4c41f64147b1133c980a8
atif00 <mohammedatif90@gmail.com>
2021-10-05 13:09:32 +0530

    ADD Create BST

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -7,7 +7,19 @@ public class BinarySearchTree {
         bTree.add(56);
         bTree.add(30);
         bTree.add(70);
+        bTree.add(22);
+        bTree.add(40);
+        bTree.add(60);
+        bTree.add(95);
+        bTree.add(11);
+        bTree.add(65);
+        bTree.add(3);
+        bTree.add(16);
+        bTree.add(63);
+        bTree.add(67);
 
+
+        System.out.println("Size of BST: "+bTree.size());
         System.out.println("#### Tree ####");
         System.out.println(bTree.inOrder() + " ");
 
